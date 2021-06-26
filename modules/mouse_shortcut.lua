acd02747b6c4279c15c92e99a95a112d66a559f2
Jake Park <qkstjr09@gmail.com>
2021-06-26 15:35:53 +0900

    마우스 방향이동 function 수정
    move_mouse_left_up() 부터 move_mouse_right_down() 까지 8개 function -> move_mouse(right, down)으로 통합

--- modules/mouse_shortcut.lua
+++ modules/mouse_shortcut.lua
@@ -1,70 +1,48 @@
 --[[ 
-키보드 조합 입력시 마우스 커서 이동
+마우스 커서 이동
 --]]
 local scale = 7 -- 기본 이동거리
-function move_mouse_left_up()
+function move_mouse(right, down)
     local pos = hs.mouse.getRelativePosition()
-    pos["x"] = pos["x"] - math.sqrt(scale*scale/2)
-    pos["y"] = pos["y"] - math.sqrt(scale*scale/2) 
+    pos["x"] = pos["x"] + right * scale
+    pos["y"] = pos["y"] + down * scale
     hs.mouse.setRelativePosition(pos)
 end
-function move_mouse_left()
-    local pos = hs.mouse.getRelativePosition()
-    pos["x"] = pos["x"] - scale
-    hs.mouse.setRelativePosition(pos)
-end
-function move_mouse_left_down()
-    local pos = hs.mouse.getRelativePosition()
-    pos["x"] = pos["x"] - math.sqrt(scale*scale/2)
-    pos["y"] = pos["y"] + math.sqrt(scale*scale/2)
-    hs.mouse.setRelativePosition(pos)
-end
-function move_mouse_up()
-    local pos = hs.mouse.getRelativePosition()
-    pos["y"] = pos["y"] - scale
-    hs.mouse.setRelativePosition(pos)
-end
-function move_mouse_right_up()
-    local pos = hs.mouse.getRelativePosition()
-    pos["x"] = pos["x"] + math.sqrt(scale*scale/2) 
-    pos["y"] = pos["y"] - math.sqrt(scale*scale/2) 
-    hs.mouse.setRelativePosition(pos)
-end
-function move_mouse_right()
-    local pos = hs.mouse.getRelativePosition()
-    pos["x"] = pos["x"] + scale
-    hs.mouse.setRelativePosition(pos)
-end
-function move_mouse_right_down()
-    local pos = hs.mouse.getRelativePosition()
-    pos["x"] = pos["x"] + math.sqrt(scale*scale/2)
-    pos["y"] = pos["y"] + math.sqrt(scale*scale/2)
-    hs.mouse.setRelativePosition(pos)
-end
-function move_mouse_down()
-    local pos = hs.mouse.getRelativePosition()
-    pos["y"] = pos["y"] + scale
-    hs.mouse.setRelativePosition(pos)
-end
+
+--[[ 
+마우스 커서 현재 모니터의 센터로 이동
+--]]
 function move_mouse_center()
     local screen = hs.mouse.getCurrentScreen()
     local rect = screen:fullFrame()
     local center = hs.geometry.rectMidPoint(rect)
     hs.mouse.absolutePosition(center)
 end
+
+--[[ 
+마우스 커서 다음 모니터의 센터로 이동
+--]]
 function move_mouse_next_monitor_center()
     local screen = hs.mouse.getCurrentScreen()
     local nextScreen = screen:next()
     local rect = nextScreen:fullFrame()
     local center = hs.geometry.rectMidPoint(rect)
     hs.mouse.absolutePosition(center)
-end
+
+--[[ 
+마우스 왼쪽 버튼 클릭
+--]]
 function mouse_left_click()
     hs.eventtap.leftClick(hs.mouse.absolutePosition())
 end
+--
+--[[ 
+마우스 오른쪽 버튼 클릭
+--]]
 function mouse_right_click()
     hs.eventtap.rightClick(hs.mouse.absolutePosition())
 end
+
 --[[
  방향이동 단축키 눌렀을때 이동하는 거리 토글 7 <-> 100
 --]]
@@ -75,14 +53,19 @@ function toggle_mouse_scale()
         scale = 7
     end
 end
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'u', move_mouse_left_up, nil, move_mouse_left_up)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'j', move_mouse_left, nil, move_mouse_left)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'm', move_mouse_left_down, nil, move_mouse_left_down)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'i', move_mouse_up, nil, move_mouse_up)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, ',', move_mouse_down, nil, move_mouse_down)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'o', move_mouse_right_up, nil, move_mouse_right_up)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'l', move_mouse_right, nil, move_mouse_right)
-hs.hotkey.bind({'⌃', '⇧', '⌘'}, '.', move_mouse_right_down, nil, move_mouse_right_down)
+
+-- 마우스 방향이동 Start
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'u', function() move_mouse(-1, -1) end , nil, function() move_mouse(-1, -1) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'j', function() move_mouse(-1,  0) end , nil, function() move_mouse(-1,  0) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'm', function() move_mouse(-1,  1) end , nil, function() move_mouse(-1,  1) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'i', function() move_mouse( 0, -1) end , nil, function() move_mouse( 0, -1) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, ',', function() move_mouse( 0,  1) end , nil, function() move_mouse( 0,  1) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'o', function() move_mouse( 1, -1) end , nil, function() move_mouse( 1, -1) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'l', function() move_mouse( 1,  0) end , nil, function() move_mouse( 1,  0) end )
+hs.hotkey.bind({'⌃', '⇧', '⌘'}, '.', function() move_mouse( 1,  1) end , nil, function() move_mouse( 1,  1) end )
+-- 마우스 방향이동 End
+
+
 hs.hotkey.bind({'⌃', '⇧', '⌘'}, 'k', mouse_left_click, nil, mouse_left_click)
 hs.hotkey.bind({'⌃', '⇧', '⌘'}, ';', mouse_right_click, nil, mouse_right_click)
 hs.hotkey.bind({'⌃', '⇧', '⌘'}, '/', toggle_mouse_scale, nil, toggle_mouse_scale)
