99f12428593a467519850d1760ca9753af124df3
Jake Park <qkstjr09@gmail.com>
2021-06-26 15:36:24 +0900

    tab size 2spaces -> 4spaces

--- modules/esc_auto_english.lua
+++ modules/esc_auto_english.lua
@@ -2,12 +2,12 @@
  `C-[`, ESC 입력시 입력소스 영어로 전환(이미 영어일겅우 제외)
 --]]
 function input_eng_and_esc()
-  local input_english = "com.apple.keylayout.ABC"
-  local input_source = hs.keycodes.currentSourceID()
-  if not (input_source == input_english) then
-    hs.keycodes.currentSourceID(input_english)
-  end
-  hs.eventtap.keyStroke({}, 'escape')
+    local input_english = "com.apple.keylayout.ABC"
+    local input_source = hs.keycodes.currentSourceID()
+    if not (input_source == input_english) then
+        hs.keycodes.currentSourceID(input_english)
+    end
+    hs.eventtap.keyStroke({}, 'escape')
 end
 
 hs.hotkey.bind({'⌃'}, 33, input_eng_and_esc)
