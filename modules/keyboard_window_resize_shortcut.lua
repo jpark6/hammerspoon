80be18c1c55c4ef5efff4bc2436d011232a9aa55
Jake Park <qkstjr09@gmail.com>
2021-06-27 10:55:50 +0900

    윈도우 resize 단축키 추가

---
+++ modules/keyboard_window_resize_shortcut.lua
@@ -0,0 +1,53 @@
+--[[
+    윈도우 모니터 이동
+--]]
+function moveWindowToMonitor(n)
+    return function()
+        local window = hs.window.focusedWindow()
+        local monitors = hs.screen.allScreens()
+        -- 맥북의 이동 또는 다른 이유로 모니터 개수 가변 여부에 따라
+        -- n%(모니터 개수) 번째 모니터로 이동
+        window:moveToScreen(monitors[(n-1) % #monitors + 1])
+    end
+end
+
+--[[
+    윈도우 창 크기 조절
+--]]
+function resize_window(left, top, width, height)
+    return function()
+        local window = hs.window.focusedWindow()
+        local frame = window:frame()
+        local screen = window:screen():frame()
+
+        frame.x = screen.x + screen.w * left
+        frame.y = screen.y + screen.h * top
+        frame.w = screen.w * width
+        frame.h = screen.h * height
+        
+        window:setFrame(frame)
+    end
+end
+
+-- 현재 윈도우 n번째 모니터로 이동Start
+hs.hotkey.bind({'⌥','⇧'}, '1',moveWindowToMonitor(1))
+hs.hotkey.bind({'⌥','⇧'}, '2',moveWindowToMonitor(2))
+hs.hotkey.bind({'⌥','⇧'}, '3',moveWindowToMonitor(3))
+hs.hotkey.bind({'⌥','⇧'}, '4',moveWindowToMonitor(4))
+hs.hotkey.bind({'⌥','⇧'}, '5',moveWindowToMonitor(5))
+hs.hotkey.bind({'⌥','⇧'}, '6',moveWindowToMonitor(6))
+hs.hotkey.bind({'⌥','⇧'}, '7',moveWindowToMonitor(7))
+hs.hotkey.bind({'⌥','⇧'}, '8',moveWindowToMonitor(8))
+hs.hotkey.bind({'⌥','⇧'}, '9',moveWindowToMonitor(9))
+-- 현재 윈도우 모니터 이동End
+-- 현재 윈도우 창 크기 조절 단축키 Start
+hs.hotkey.bind({'⌥','⇧'}, 'U', resize_window( 0, 0,.5,.5))
+hs.hotkey.bind({'⌥','⇧'}, 'J', resize_window( 0, 0,.5, 1))
+hs.hotkey.bind({'⌥','⇧'}, 'M', resize_window( 0,.5,.5,.5))
+hs.hotkey.bind({'⌥','⇧'}, 'I', resize_window( 0, 0, 1,.5))
+hs.hotkey.bind({'⌥','⇧'}, 'K', resize_window( 0, 0, 1, 1))
+hs.hotkey.bind({'⌥','⇧'}, ',', resize_window( 0,.5, 1,.5))
+hs.hotkey.bind({'⌥','⇧'}, 'O', resize_window(.5, 0,.5,.5))
+hs.hotkey.bind({'⌥','⇧'}, 'L', resize_window(.5, 0,.5, 1))
+hs.hotkey.bind({'⌥','⇧'}, '.', resize_window(.5,.5,.5,.5))
+-- 현재 윈도우 창 크기 조절 단축키 End
