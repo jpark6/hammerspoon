e923062e2b3d7b44c5ed876474abcb09cd292ba5
Jake Park <qkstjr09@gmail.com>
2021-06-27 10:54:34 +0900

    hello.lua 삭제 및 주석 한글로 변경

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
--- Hello Me?
-require('modules.hello')
 -- esc, C-[ 입력시 영어 전환
 require('modules.esc_auto_english')
 -- 한영 전환시 전환된 언어 표시
 require('modules.input_source_alert')
--- Mouse Move
+-- 키보드로 마우스 커서 이동
 require('modules.mouse_shortcut')
--- Keyboard Shortkut
+-- 키보드 단축키
 require('modules.keyboard_shortcut')
+-- 키보드 앱 열기 단축키
+require('modules.keyboard_open_apps_shortcut')
+-- 키보드 창 크기 조절 단축키
+require('modules.keyboard_window_resize_shortcut')
 
